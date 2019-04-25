858038606f4401e2e0dadb11644bd18b15d311fc
Hiroshi Sugawara <5000164@users.noreply.github.com>
2019-04-25 15:22:29 +1000

    chore: to arrange Components

--- src/templates/blog-list-template.js
+++ src/templates/blog-list-template.js
@@ -2,7 +2,7 @@ import React from "react"
 import { graphql, Link } from "gatsby"
 import styled from "styled-components"
 import moment from "moment"
-import Layout from "../components/layout"
+import Layout from "../components/Layout"
 import SEO from "../components/SEO"
 
 export default class BlogList extends React.Component {
--- src/templates/blog-template.js
+++ src/templates/blog-template.js
@@ -1,7 +1,7 @@
 import React from "react"
 import { graphql, Link } from "gatsby"
 import styled, { createGlobalStyle } from "styled-components"
-import Layout from "../components/layout"
+import Layout from "../components/Layout"
 import SEO from "../components/SEO"
 
 export default class Blog extends React.Component {
