2c6d554a2d341a25ed76d0db62bbd3b05cb06a55
Hiroshi Sugawara <5000164@users.noreply.github.com>
2021-01-05 16:29:41 +0900

    chore: Update profile text

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,7 +8,7 @@ module.exports = {
     description: `This blog is written about my life include technology.`,
     name: "菅原 浩",
     profile:
-      "Web プログラマー。好きな言葉は「安定的に不安定」。オーストラリアで英語を勉強した後に、現在デンマークで幸せについて考えている。",
+      "Web プログラマー。 好きな言葉は「安定的に不安定」。いろんな会社で働いた後、オーストラリアで英語を勉強し、デンマークで幸せについて考えて、帰国後は海の近くで生活中。",
     consent: {
       text: "このウェブサイトでは Cookie を使用します。<a href='/privacy/'>詳細はこちら</a>",
       accept: "同意する",
